95e9d111a345cad5ccbf97623e402ddd0cc1cede
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-11-11 18:40:23 -0300

    [Update] Modificacion Modelos
    Agregadas clases usuario y Supervisor y Cobrador (Herencia)

--- users/models.py
+++ users/models.py
@@ -3,7 +3,32 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
-    # add additional fields in here
+    rut_usuario = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return self.username
+
+class Supervisor(CustomUser):
+	id_supervisor = models.IntegerField(null=False)
+
+	def asignar_Ruta(Cobrador):
+   		return True;
+   	def eliminar_visita():
+   		return True;
+   	def modificar_visita():
+   		return True;
+   	def agregar_visita():
+   		return True;
+   	def asignar_perfil():
+   		return True;
+   	def asignar_usuario():
+   		return True;
+   	def modificar_usuario():
+   		return True;
+   	def eliminar_usuario():
+   		return True;
+
+class Cobrador(CustomUser):
+	id_cobrador = models.IntegerField(null=False)
+	def MostrarRutas():
+   		return True;
