ce70240a40774be793c7b04c7fde9da9e91f5484
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-11-15 11:52:42 -0300

    [Fix] - Dashboard

--- ElLugarFinal/settings.py
+++ ElLugarFinal/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ElLugarFinal.wsgi.application'
 
 LOGIN_REDIRECT_URL = 'Dashboard'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = ''
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
